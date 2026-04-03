2a38e8b75360385dd5a13010347f1ae26bcfae6c
Tommy20304 <160166323+Tommy20304@users.noreply.github.com>
2026-04-03 15:28:36 -0400

    quitando un ´ del valore.imagen

--- src/paginas/exportar.vue
+++ src/paginas/exportar.vue
@@ -84,7 +84,7 @@ onUnmounted(() => {
         @click="abrirModal(valores)"
       >
         <div class="imagen-wrapper">
-          <img :src="valores.imagen`" :alt="nombre">
+          <img :src="valores.imagen" :alt="nombre">
           <div class="overlay-simple">Ver detalles</div>
         </div>
         <div class="info-basica">
